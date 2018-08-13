7469cccb9d0932a58d59351c13bc4fcd21a67664
Alessandro Segala <ItalyPaleAle@users.noreply.github.com>
2018-08-12 19:05:34 -0700

    auth.js sample format

--- test/data/auth.js
+++ test/data/auth.js
@@ -1,12 +1,32 @@
 'use strict'
 
+/* eslint-disable no-unreachable */
+
+// DELETE THIS LINE AFTER SETTING AUTH INFO
+throw Error('Missing authorization info for cloud providers')
+
 module.exports = {
     // Minio
-    minio: {
+    'minio': {
         endPoint: 'localhost',
         port: 9001,
         secure: false,
         accessKey: '8O683FBOQPTVBLX8T11M',
         secretKey: 'ulTofsUnwmgnYLR8I6D4IbFD9N/NJ+XJ0X84bxrH'
+    },
+
+    // Azure
+    'azure-storage': 'DefaultEndpointsProtocol=https;AccountName=...;AccountKey=...;EndpointSuffix=core.windows.net',
+
+    // AWS
+    'aws-s3': {
+        accessKey: '...',
+        secretKey: '...'
+    },
+
+    // Google Cloud
+    'google-cloud-storage': {
+        projectId: '...',
+        keyFilename: '/path/to/file.json'
     }
 }
