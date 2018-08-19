0587770d913a0b49f37fe5dbfc7f9da35e52e50c
Alessandro Segala <ItalyPaleAle@users.noreply.github.com>
2018-08-19 13:28:26 -0700

    Added docs for all modules, finally

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -2,14 +2,15 @@
 
 # Run TypeDoc
 
+#  --excludePrivate \
+#  --excludeProtected \
+
 ./node_modules/.bin/typedoc \
   --name SMCloudStore \
   --out docs/ \
   --readme README.md \
   --target ES6 \
   --module commonjs \
-  --excludePrivate \
-  --excludeProtected \
   --plugin typedoc-plugin-monorepo \
   --external-modulemap  ".*\/packages\/([\\w\\-_]+)\/" \
   --mode modules \
